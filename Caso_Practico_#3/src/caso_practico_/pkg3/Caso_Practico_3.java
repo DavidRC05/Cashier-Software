0f7d43e05bb488e8933e5b64e5b3133dabbeb2ed
Arturo <Arturo@amongus-pc>
2022-12-08 21:07:32 -0600

    que

---
+++ Caso_Practico_#3/src/caso_practico_/pkg3/Caso_Practico_3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caso_practico_.pkg3;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Caso_Practico_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menuinter menu = new Menuinter();
+        menu.setVisible(true);
+    }
+    
+}
